84ce8a3e405c85b5ba6a5de6c3b6fe8b6ae65b31
Bikash Ghimire <bikashghimire@Bikashs-MacBook-Air.local>
2023-11-17 14:55:38 +0545

    day-1 js comp

---
+++ datatypes-1/script.js
@@ -0,0 +1,67 @@
+// 2 types of data types 
+// 1. Primitive  : Number,String,Boolean,Null,Undefined,Symbol
+// 2. Non-Primitive  : Object and array 
+
+// boolean ex 
+
+// let num1 = 2
+// let num2 = 2
+// console.log(num1===num2)      //true
+
+
+// non primitive ex 
+
+// let nums = [1,2,3];
+// nums[0]=10+10;
+// console.log(nums)    // 20 ,2,3
+
+
+// let nums = [1,2,3];
+// let numbers = nums;
+// console.log(nums===numbers);  // true
+
+
+// concat
+// let name = "Bikash"
+// let caste = " Ghimire"
+// let fullName = name + caste
+// console.log(fullName); // Bikash Ghimire
+
+
+// template literals  ${} 
+
+// let num1 = 2
+// let num2 = 3 
+// console.log(`The sum of ${num1} and ${num2} is ${num1+num2}`) // The sum of 2 and 3 is 5 
+
+// const a = 2
+// const b = 3
+// console.log(`${a} is lesser than ${b}`)  // 2 is lesser than 3 
+
+
+// length 
+// const js = "JavaScript"
+// console.log(js.length);   // 10 
+
+// to UpperCase  () 
+// const js = "javascript"
+// console.log(js.toUpperCase());    // JAVASCRIPT
+
+
+// toLowerCase
+// const js = "javascript"
+// console.log(js.toLowerCase());    // javascript
+
+
+// split () method   --> Chnages into array 
+
+// const spilt = '30 days of JS' 
+// console.log(spilt.split());  // ['30 days of JS']
+
+// const spilt = '30 days of JS' 
+// console.log(spilt.split(' '));  // ['30 days of JS']
+
+
+// trim [ removes the spaces in the begining or at the ending]
+// const trim = " hello world "
+// console.log(trim.trim(' '));     // hello world 
