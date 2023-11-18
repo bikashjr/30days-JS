2c1e070445b3f5ea350814e7da1c6347e6db8a26
Bikash Ghimire <bikashghimire@Bikashs-MacBook-Air.local>
2023-11-18 13:36:29 +0545

    solved day2 challanges

--- datatypes-1/script.js
+++ datatypes-1/script.js
@@ -8,6 +8,10 @@
 // let num2 = 2
 // console.log(num1===num2)      //true
 
+// let num1 = 2
+// let num2 = 3
+// console.log(num1===num2)      //false
+
 
 // non primitive ex 
 
@@ -28,7 +32,7 @@
 // console.log(fullName); // Bikash Ghimire
 
 
-// template literals  ${} 
+// template literals  `${}` 
 
 // let num1 = 2
 // let num2 = 3 
@@ -59,9 +63,63 @@
 // console.log(spilt.split());  // ['30 days of JS']
 
 // const spilt = '30 days of JS' 
-// console.log(spilt.split(' '));  // ['30 days of JS']
+// console.log(spilt.split(' '));  // ['30' 'days' 'of' JS']
 
 
 // trim [ removes the spaces in the begining or at the ending]
 // const trim = " hello world "
 // console.log(trim.trim(' '));     // hello world 
+
+//  Day 2 challanges
+
+// const challenge = '30 Days Of JavaScript'
+// console.log(challenge.split());
+
+
+// const challanges1 = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+// const challanges2 = challanges1;
+// console.log(challanges2.split(" "));  
+
+
+// const challanges3 = '30 days of JavaScript'
+// const replace = challanges3.replace('JavaScript','Python')
+// console.log(replace);  // '30 days of Python'
+
+
+// const challanges3 = '30 days of JavaScript'
+// console.log(challanges3.charAt(15));   // J
+
+
+// const challanges3 = '30 days of JavaScript'
+// console.log(challanges3.charCodeAt('J'));  // 51
+
+// let phrase = "30 Days Of JavaScript";
+// let position = phrase.indexOf("a");
+
+// console.log("Position of 'a':", position);  // 4 
+
+// let phrase = "30 Days Of JavaScript"
+// let solve = phrase.lastIndexOf('a')
+// console.log("Position of a :", solve);
+
+
+// const result = 'You cannot end a sentence with because because because is a conjunction'
+// const solve  = result.search('because');
+// console.log('The word because is in ' + solve + " letter" ); //The word is in 31 letter
+
+
+// let time = ' 30 days '
+// let sub = 'js'
+// let timeSub = time + sub ;
+// console.log( timeSub);  // 30 days of js
+
+
+// const js = '30 Days Of JavaScript' 
+// console.log(js.repeat( 2));
+
+
+// const js = '30 Days Of JavaScript'
+// console.log(js.slice(2));
+
+// const jss = '30 Days Of JavaScript'
+// console.log(jss.substring(2));
