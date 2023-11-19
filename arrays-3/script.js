b3bceb04131a3026e686abe76d349278a844bd6c
Bikash Ghimire <bikashghimire@Bikashs-MacBook-Air.local>
2023-11-19 19:54:36 +0545

    comp day 5

---
+++ arrays-3/script.js
@@ -0,0 +1,171 @@
+// Arrays 
+
+
+// Array constructor
+// const arr = Array() 
+// console.log(arr);   // creates an empty array 
+
+
+// const numbers = [ 1,2,3,4,5,6]
+// console.log(numbers.length);
+
+
+// const arr = [
+//     'Bikash',
+//     'Apple',
+//     { skills: ['html', 'CSS'] },
+//     {country : 'United States',city:'ktm'}
+// ]
+// console.log(arr);
+
+
+// const js = 'Javascript'
+// const py = js.split('  ')
+// console.log(py);
+
+
+// chnaging the index name 
+// const listFruits = ['apple','banana','mango']
+// console.log(listFruits[0]='mango');
+
+
+// const eightValues = Array(8).fill('y')
+// console.log(eightValues);
+
+
+// const twoValues = Array(2).fill('B')
+// console.log(twoValues);
+
+
+// To concatenate two arrays.
+// const arr1 = [1,2,3]
+// const arr2 = [4,5,6]
+// const arr3 = arr1.concat(arr2)
+// console.log(arr3);
+
+
+// const fruits = ['apple', 'banana', 'mango']
+// const index = fruits.indexOf('apple')
+// index === -1 ? console.log('It exits') : console.log('Not exists');
+// console.log(index);
+
+
+// const fruits = ['apple', 'banana', 'mango']
+// const fruitsTostring = fruits.toString()
+// console.log(fruitsTostring);
+
+// slice method 
+// const names = ['tom', 'champ', 'ram', 'shyam', 'gita']
+// console.log(names.slice(0, 2));  // tom,champ is included but ram is not included 
+// slice methods takes the first index but doesnt takes the last index
+
+
+
+// const nums = [1, 2, 3, 4, 5]
+// console.log(nums.slice(1, 4));
+
+
+// splice method
+// const name = ['tom', 'champ', 'ram', 'shyam']
+// name.splice(1, 2, 'HI')  // inplace of index of 1,2 (Hi) overtakes it 
+// console.log(name);
+
+
+// const pushArr = [1, 2, 3, 4, 5]
+// pushArr.push(6)
+// console.log(pushArr);
+
+
+// const fruits = ['apple', 'orange', 'mango']
+// // fruits.push('guava')
+// console.log(fruits.slice(0,2));
+
+
+// pop :  //removes the last element
+
+// const nums= [1,2,3,4,5,6]
+// nums.pop()
+// console.log(nums);  // 1,2,3,4,5
+
+// shift :  removes the begining element
+
+// const nums = [1,2,3,4,5,6]
+// nums.shift()
+// console.log(nums);
+
+// unshift  adds the element to the starting position
+// const nums = [1,2,3,4,5,6]
+// nums.unshift(0)
+// console.log(nums);
+
+// reverse 
+// const numbers = [1,2,3,4,5]
+// numbers.reverse()
+// console.log(numbers);
+
+
+1.// empty arr 
+// const arr = Array()
+// console.log(arr);
+
+2.
+// const arrays = [1,2,3,4,5]
+// // console.log(arrays.length);
+// console.log(arrays[0]);
+// console.log(arrays[2]);
+// console.log(arrays[4]);
+
+
+// const mixedDataTypes = [1, "Hello", 3.14, true, { key: "value" }];
+// const arrayLength = mixedDataTypes.length;
+
+// console.log("Mixed Data Types Array:", mixedDataTypes);
+// console.log("Length of the Array:", arrayLength);
+
+
+// const itCompanies = ["facebook", "Google", "Microsoft", 'Apple', 'IBM', 'Oracle', 'Amazon']
+// console.log('There are :',itCompanies.length, 'ITcompany' );
+// console.log(itCompanies[0]);
+// console.log(itCompanies[3]);
+// console.log(itCompanies[6]);
+
+
+
+// const itCompanies = ["facebook", "Google", "Microsoft", 'Apple', 'IBM', 'Oracle', 'Amazon']
+// console.log(itCompanies.includes('facebook'));
+// if(itCompanies.includes('Google')){
+//     console.log('Company Found');
+// }
+// else{
+//     console.log('Not found');
+// }
+
+// itCompanies.includes("Google") ? console.log('Company found') : console.log('Company not found')
+
+
+// const itCompanies = ["facebook", "Google", "Microsoft", 'Apple', 'IBM', 'Oracle', 'Amazon']
+// itCompanies.reverse()
+// console.log(itCompanies.splice(0,4));
+
+// const itCompanies = ["facebook", "Google", "Microsoft", 'Apple', 'IBM', 'Oracle', 'Amazon']
+// console.log(itCompanies.sort());
+
+// const itCompanies = ["facebook", "Google", "Microsoft", 'Apple', 'IBM', 'Oracle', 'Amazon']
+// console.log(itCompanies.toString().toUpperCase());
+
+// const itCompanies = ["facebook", "Google", "Microsoft", 'Apple', 'IBM', 'Oracle', 'Amazon']
+// console.log(itCompanies.slice(3));
+
+// to remove the first elemnt from the array ... 
+// const itCompanies = ["facebook", "Google", "Microsoft", 'Apple', 'IBM', 'Oracle', 'Amazon']
+// itCompanies.shift(0);
+// console.log(itCompanies);
+
+// const itCompanies = ["facebook", "Google", "Microsoft", 'Apple', 'IBM', 'Oracle', 'Amazon']
+// console.log(itCompanies.slice(0,3,4,5));
+
+const itCompanies = ["facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+
+itCompanies.splice(0, itCompanies.length);
+
+console.log(itCompanies);
---
+++ conditional-2/script.js
@@ -0,0 +1,126 @@
+// let num = 3
+// if (num > 0) {
+//     console.log(`${num} is a odd number`)
+// }
+
+// let isRaining = true ;
+// if(isRaining){
+//     console.log('Wear warm cloth');
+// }
+
+// let a = 0 ;
+// if(a<0){
+//     console.log('a is lesser')
+// }else if(a>0){
+//     console.log('A is greater');
+// }
+// else{
+//     console.log('A is zeroo');
+// }
+
+
+// Switch Case 
+// let weather = "cloudy";
+// switch(weather){
+//     case 'summer':
+//         console.log('summer weather'); 
+//         break;
+//         case 'windy':
+//             console.log('Windy weather');
+//             break;
+//             default:
+//                 console.log(`no weather`);
+// }
+
+// let num = 10;
+// switch (num) {
+//     case 1:
+//         console.log('1 is the output');
+//         break;
+//     case 10:
+//         console.log('Equal to 10 ');
+//         break;
+//     default:
+//         console.log('not equal to 10');
+// }
+
+
+// let num = prompt('Enter number');
+// switch (true) {
+//   case num > 0:
+//     console.log('Number is positive');
+//     break;
+//   case num == 0:
+//     console.log('Numbers is zero');
+//     break;
+//   case num < 0:
+//     console.log('Number is negative');
+//     break;
+//   default:
+//     console.log('Entered value was not a number');
+// }
+
+//Ternary operator
+
+// let isRaining = 'true';
+// isRaining ?
+//     console.log('Raining') :
+//     console.log("Not Raining");
+
+
+// let num = 35;
+// num >=15 ?
+// console.log(`Num is greater than ${num}`) : 
+// console.log(`Num is less than ${num}`)
+
+
+// challanges
+
+1.
+// Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+
+// const age = prompt('Please enter your age');
+
+// if(age>18 || age>=18){
+//    alert('You are old enough to drive');
+// }else{
+//    alert(`Wait for the number of years he needs to turn 18`);
+// }
+
+
+// 2. 
+
+// Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older(me or you).Use prompt(“Enter your age:”) to get the age as input.
+
+// const myAge = prompt('Enter my age');
+// const yourAge = prompt('Enter your age');
+
+// if (myAge > yourAge) {
+//     alert('I am bigger than you');
+// }
+// else if (myAge < yourAge) {
+//     alert('I am smaller than you');
+// }
+// else if (myAge === yourAge) {
+//     alert('have equal ages');
+// }
+// else {
+//    alert('We have a huge age difference');
+// }
+
+
+// let a = 3
+// let b = 4 
+// a > b ? 
+// console.log(`${a} is > than ${b}`) 
+// : console.log(`${a} is < ${b}`)
+
+
+// function isEven(number) {
+//     return number % 2 === 0;
+//   }
+  
+//   console.log(isEven(4)); // true (4 is even)
+//   console.log(isEven(7)); // false (7 is not even)
+//   console.log(isEven(0)); // true (0 is even)
